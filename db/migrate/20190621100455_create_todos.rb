5eebf7c9fe915aad81c5c155d3e26ed2ddd9a821
Marta Tuśka Krajewska <marta.t.krajewska@gmail.com>
2019-06-21 23:29:43 +0200

    Adding User model with tests and factory, adding bycript gem, fixing migrations of creating todos and items

--- db/migrate/20190621100455_create_todos.rb
+++ db/migrate/20190621100455_create_todos.rb
@@ -1,8 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
-      t.string :title, null: false
-      t.string :created_by, null: false
+      t.string :title
+      t.string :created_by
 
       t.timestamps
     end
--- db/migrate/20190621100602_create_items.rb
+++ db/migrate/20190621100602_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name, null: false
-      t.boolean :done, null: false
+      t.string :name
+      t.boolean :done
       t.references :todo, foreign_key: true
 
       t.timestamps
