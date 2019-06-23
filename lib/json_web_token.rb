d7ea0d816b24c1c311146ba211cbd86f3fd2c0da
Marta Tuśka Krajewska <marta.t.krajewska@gmail.com>
2019-06-23 19:56:54 +0200

    Fixing error in json_web_token, fixing error with exception_handler, addinglib to autoload_path

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,6 @@ class JsonWebToken
   # secret to encode and decode token
   HMAC_SECRET = Rails.application.secrets.secret_key_base
 
-  # payload = user.id
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from creation time
     payload[:exp] = exp.to_i
@@ -14,8 +13,8 @@ class JsonWebToken
     # get payload; first index in decoded Array
     body = JWT.decode(token, HMAC_SECRET)[0]
     HashWithIndifferentAccess.new body
-    # rescue from all decode errors
-  rescue JWT::DecodeError =>
+      # rescue from all decode errors
+  rescue JWT::DecodeError => e
     # raise custom error to be handled by custom handler
     raise ExceptionHandler::InvalidToken, e.message
   end
